7fd568ca451bd70d188e8594f1cfcbef3c7ace6c
Suraj Telrandhe <suraj.telrandhe16@gmail.com>
2025-05-08 22:40:44 +0530

    Fix update_record view to use GET method and rename update_record function to update_status for clarity

--- website/views.py
+++ website/views.py
@@ -95,7 +95,7 @@ def add_record(request):
 def update_record(request, pk):
     if request.user.is_authenticated and request.user.is_superuser:
         current_record = Record.objects.get(id = pk)
-        form = AddRecordForm(request.POST or None, instance=current_record)
+        form = AddRecordForm(request.GET or None, instance=current_record)
         if form.is_valid():
             form.save()
             messages.success(
@@ -108,7 +108,7 @@ def update_record(request, pk):
         return redirect('home')
 
 
-def update_record(request, pk):
+def update_status(request, pk):
     if request.user.is_authenticated and request.user.is_superuser:
         current_record = Record.objects.get(id = pk)
         current_record.status = not current_record.status
